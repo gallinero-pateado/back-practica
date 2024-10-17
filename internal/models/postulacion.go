533a2923d401c1cde2dc443e516fada1dc7aadc0
JaimeFigueroaP22 <jfigueroap@utem.cl>
2024-10-17 15:53:55 -0300

    modelo para postular a practicas

---
+++ internal/models/postulacion.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+)
+
+type Postulacion struct {
+	Id                    uint      `gorm:"primaryKey;autoIncrement"`
+	Id_usuario            int       `json:"id_usuario"`
+	Fecha_postulacion     time.Time `json:"default:CURRENT_TIMESTAMP"`
+	Mensaje               string    `json:"mensaje"`
+	Id_estado_postulacion int       `json:"id_estado_publicacion"`
+}
+
+// TableName establece el nombre de la tabla para GORM
+func (Postulacion) TableName() string {
+	return "postulacion"
+}
